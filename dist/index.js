5c9bdf71b52100e1f1bc333bf5fa1910992c7c9e
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-12-06 21:02:50 +0100

    Deploy

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 var _a;
 import { PORT } from './config.js';
-import { setupServer } from './server.js';
+import { NOT_FOUND, setupServer } from './server.js';
 import { PGDatabase } from './pg-database.js';
 let database = new PGDatabase();
 const setup = setupServer();
@@ -18,12 +18,26 @@ setup.get('/item', (request) => __awaiter(void 0, void 0, void 0, function* () {
     const type = (_b = request.query.type) === null || _b === void 0 ? void 0 : _b.split('|');
     return database.itemsWithSpecification({ progress: 'notStarted', parent: null, type });
 }));
+setup.get('/item/:id', (request) => __awaiter(void 0, void 0, void 0, function* () {
+    var _c;
+    const id = request.params.id;
+    return (_c = yield database.item(id)) !== null && _c !== void 0 ? _c : NOT_FOUND;
+}));
+setup.get('/item/:id/child', (request) => __awaiter(void 0, void 0, void 0, function* () {
+    var _d;
+    const id = request.params.id;
+    const type = (_d = request.query.type) === null || _d === void 0 ? void 0 : _d.split('|');
+    return database.itemsWithSpecification({ progress: 'notStarted', parent: id, type });
+}));
 const server = setup.finalize();
-server.listenAtPort((_a = parseInt(PORT !== null && PORT !== void 0 ? PORT : '80')) !== null && _a !== void 0 ? _a : 80);
+const port = (_a = parseInt(PORT !== null && PORT !== void 0 ? PORT : '80')) !== null && _a !== void 0 ? _a : 80;
+server.listenAtPort(port);
 process.stdout.write(`\x1B[35mListening on port \x1B[30m${PORT !== null && PORT !== void 0 ? PORT : '80'}\x1B[0m\n\n`);
-export function close() {
-    server.stopListening();
-}
-export function overrideDatabase(testDatabase) {
+export function start(testDatabase) {
     database = testDatabase;
+    server.stopListening();
+    server.listenAtPort(port);
+}
+export function stop() {
+    server.stopListening();
 }
--- dist/pg-database.js
+++ dist/pg-database.js
@@ -7,9 +7,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { DATABASE_CONNECTION_STRING } from './config.js';
 import pg from 'pg';
+import { DATABASE_CONNECTION_STRING } from './config.js';
 export class PGDatabase {
+    item(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const db = new pg.Client(DATABASE_CONNECTION_STRING);
+            yield db.connect();
+            const rs = yield db.query('SELECT * FROM Items WHERE id = $1', [id]);
+            const result = rs.rows;
+            yield db.end();
+            return result.map(r => {
+                var _a;
+                return ({
+                    id: r.id,
+                    type: r.type,
+                    title: r.title,
+                    progress: r.progress,
+                    parentId: (_a = r.parent_id) !== null && _a !== void 0 ? _a : undefined,
+                });
+            })[0];
+        });
+    }
     itemsWithSpecification(specification) {
         return __awaiter(this, void 0, void 0, function* () {
             const clause = whereClause(specification);
