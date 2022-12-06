67f5bf16ce7e557c64a8a65341c4764c4851198c
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-12-06 08:36:49 +0100

    Deploy changes

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,29 @@
-import express from 'express';
-import { json, Router, urlencoded } from 'express';
-import { createServer } from 'http';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var _a;
 import { PORT } from './config.js';
-const app = express();
-app.use(json());
-app.use(urlencoded({ extended: false }));
-const router = Router();
-router.get('/', (_, res) => {
-    res.json({ message: 'alive' });
-});
-app.use('/', router);
-const server = createServer(app);
-server.listen(PORT);
-process.stdout.write(`\x1B[35mListening on port \x1B[30m${PORT}\x1B[0m\n\n`);
+import { setupServer } from './server.js';
+import { PGDatabase } from './pg-database.js';
+let database = new PGDatabase();
+const setup = setupServer();
+setup.get('/item', (request) => __awaiter(void 0, void 0, void 0, function* () {
+    var _b;
+    const type = (_b = request.query.type) === null || _b === void 0 ? void 0 : _b.split('|');
+    return database.itemsWithSpecification({ progress: 'notStarted', parent: null, type });
+}));
+const server = setup.finalize();
+server.listenAtPort((_a = parseInt(PORT !== null && PORT !== void 0 ? PORT : '80')) !== null && _a !== void 0 ? _a : 80);
+process.stdout.write(`\x1B[35mListening on port \x1B[30m${PORT !== null && PORT !== void 0 ? PORT : '80'}\x1B[0m\n\n`);
 export function close() {
-    server.close();
+    server.stopListening();
+}
+export function overrideDatabase(testDatabase) {
+    database = testDatabase;
 }
---
+++ dist/pg-database.js
@@ -0,0 +1,61 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { DATABASE_CONNECTION_STRING } from './config.js';
+import pg from 'pg';
+export class PGDatabase {
+    itemsWithSpecification(specification) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const clause = whereClause(specification);
+            const query = clause ? `SELECT * FROM Items WHERE ${clause}` : 'SELECT * FROM Items';
+            const db = new pg.Client(DATABASE_CONNECTION_STRING);
+            yield db.connect();
+            const rs = yield db.query(query, parameters(specification));
+            const result = rs.rows;
+            yield db.end();
+            return result.map(r => {
+                var _a;
+                return ({
+                    id: r.id,
+                    type: r.type,
+                    title: r.title,
+                    progress: r.progress,
+                    parentId: (_a = r.parent_id) !== null && _a !== void 0 ? _a : undefined,
+                });
+            });
+        });
+    }
+}
+function whereClause(specification) {
+    const parameters = ['']; // Create a 1-based array by placing nonsense in position 0
+    if (specification.progress)
+        parameters.push('progress');
+    if (specification.parent)
+        parameters.push('parent');
+    if (specification.type)
+        parameters.push('type');
+    const result = [];
+    if (specification.progress)
+        result.push(`progress = ANY($${parameters.indexOf('progress')}::TEXT[])`);
+    if (specification.parent === null)
+        result.push('parent_id IS NULL');
+    if (specification.parent)
+        result.push(`parent_id = $${parameters.indexOf('parent')}`);
+    if (specification.type)
+        result.push(`type = ANY($${parameters.indexOf('type')}::TEXT[])`);
+    return result.join(' AND ');
+}
+function parameters(specification) {
+    const progressIn = toArray(specification.progress);
+    const typeIn = toArray(specification.type);
+    return [progressIn, specification.parent, typeIn].filter(p => p);
+}
+function toArray(value) {
+    return typeof value === 'string' ? [value] : value;
+}
